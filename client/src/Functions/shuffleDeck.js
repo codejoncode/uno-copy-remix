bd21fd8d384634105ff6cf6f098070a4e5c09da4
Jonathan <jonathanjholloway@gmail.com>
2019-07-12 13:09:31 -0500

    added comments to the shuffle deck function

--- client/src/Functions/shuffleDeck.js
+++ client/src/Functions/shuffleDeck.js
@@ -1,4 +1,9 @@
 const shuffleDeck = (deck) => {
+    /**
+     * This function works in big O of n  run time using the swapping technique to iterate over the list and swap out cards 
+     * This will shuffle the deck in the most optimal time available. 
+     * returns the shuffled deck 
+     */
     for (let i = deck.length - 1; i > 0; i--){
       const j = Math.floor(Math.random() * (i + 1));
       const temp = deck[i];

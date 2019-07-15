e09a1db2b5ff15fb98cd1debe9e5132340335ca6
Jonathan <jonathanjholloway@gmail.com>
2019-07-15 15:11:03 -0500

    added the remove by value to the linkedlist class and started working on a next player turn helper

--- client/src/Classes/LinkedList.js
+++ client/src/Classes/LinkedList.js
@@ -7,54 +7,61 @@
  */
 
 class LinkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null; 
-        this.count = 0; 
+  constructor(){
+      this.head = null;
+      this.tail = null; 
+      this.count = 0; 
+  }
+
+  append (value) {
+    const newNode = {};
+    newNode.value = value; 
+    if(this.count === 0){
+      this.head = newNode;
+      this.head.next = newNode;
+      this.tail = this.head;
+      this.head.previous = this.tail;
+      this.tail.previous = this.head; 
+    } else {
+      newNode.previous = this.tail; 
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.tail.next = this.head; // round and round
+      //this connects the head to the tail makes it go round and round as well. 
+      this.head.previous = this.tail; 
     }
-
-    append (value) {
-      const newNode = {};
-      newNode.value = value; 
-      if(this.count === 0){
-        this.head = newNode;
-        this.head.next = newNode;
-        this.tail = this.head;
-        this.head.previous = this.tail;
-        this.tail.previous = this.head; 
-      } else {
-        newNode.previous = this.tail; 
-        this.tail.next = newNode;
-        this.tail = newNode;
-        this.tail.next = this.head; // round and round
-        this.head.previous = this.tail; 
-      }
-      this.count++;
+    this.count++;
+  }
+  remove(value){
+    /**
+     * search for value then remove it 
+     */
+    let current = this.head; 
+    //it's a circular linked list so this will loop forever if the value doesn't exist 
+    let checked = 0; //increment each time we go next 
+    while(current.value != value || checked < this.count){
+      current = current.next; 
+      checked++; 
     }
+    //at this point because I avoided the infinite loop I still have to check if the value of current matches the value i'm looking for. 
+    if(current.value === value){
+      //we have it
+      //so the previous  next would be the current but now set it to current.next  taking current out of the equation 
+      current.previous.next = current.next;  
+      this.count--;
+      return true; //
+    } else 
+    return null; 
+
+  }
 }
+export default LinkedList; 
+/*What makes this a circularl 
+Circular Linked List is a variation of Linked list in which the first element points to the last element and the last element points to the first element. Both Singly Linked List and Doubly Linked List can be made into a circular linked list.
 
-const newList = new LinkedList(); 
+What makes this a doubly linked list is there is a previous and next option on each individual node. Meaning we can go forward or back.    
+*/
 
-newList.append(0);
-newList.append(1);
-newList.append(2);
-newList.append(3);
-newList.append(4);
-newList.append(5);
-newList.append(6);
-newList.append(7);
-newList.append(8);
-newList.append(9);
+//https://repl.it/@codejoncode/circular-linked-list-with-removal-function  used to test  
 
-let keepTrack = 0;
-// let current = newList.head;
-let current = newList.tail; 
-while(keepTrack < 100){
-  console.log(current.value);
-  // current = current.next; 
-  current = current.previous
-  keepTrack += 1; 
-}
-//goes round and round   
-//uses previous and next just fine; 
 
